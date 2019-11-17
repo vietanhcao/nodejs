c98cd2bd98765a4fe0057b8adb56a72a85b66034
vietanhcao <vietanhcao1994@gmail.com>
2019-11-17 23:21:55 +0700

    add cookie and save it

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,15 +3,21 @@ import { RequestHandler } from 'express';
 
 
 export const getLogin: RequestHandler = async (req,res,next) => {
+  const dataCookie = req.get('Cookie');
+  const regex = /loggedIn=/g;
+  let isLoginIn: Boolean = false;
+  if (regex.test(dataCookie)){
+    isLoginIn = JSON.parse(dataCookie.split('loggedIn=')[1]) //convert to boolean
+  }
   res.render('auth/login',{
     // orders,
     pageTitle: 'Login',
     path: '/login',
-    isAuthenticated: (req as any).isLoginIn
+    isAuthenticated: isLoginIn
   });
 };
 
 export const postLogin: RequestHandler = async (req,res,next) => {
-  (req as any).isLoginIn = true;
+  res.setHeader('Set-Cookie','loggedIn=true');
   res.redirect('/');
 };
