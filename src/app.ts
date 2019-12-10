3642c803b7fbf0b3601935d5c0281b57b7403566
vietanhcao <vietanhcao1994@gmail.com>
2019-12-10 11:54:05 +0700

    add more status codes

--- src/app.ts
+++ src/app.ts
@@ -41,8 +41,14 @@ app.use(
 );
 app.use(csurfProtection);
 app.use(flash());
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn; //You are passing the  object into the pug template
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
 
 app.use(async (req, res, next) => {
+	// throw new Error('Dummy');
 	try {
 		if (!req.session.user) {
 			return next();
@@ -53,15 +59,9 @@ app.use(async (req, res, next) => {
 		}
 		(req as any).user = user;
 	} catch (error) {
-		throw new Error(error);
+		next(new Error(error));
 	}
 
-	next();
-});
-
-app.use((req, res, next) => {
-	res.locals.isAuthenticated = req.session.isLoggedIn; //You are passing the  object into the pug template
-	res.locals.csrfToken = req.csrfToken();
 	next();
 });
 
@@ -74,7 +74,8 @@ app.get('/500', errorControllers.get500Page);
 app.get('/404', errorControllers.get404Page);
 app.use((error, req, res, next) => {
 	// res.status(error.httpStatusCode)
-	res.redirect('/500');
+	// res.redirect('/500');
+	res.status(500).render('500', { pageTitle: 'Error!', path: '500', isAuthenticated: req.session.isLoggedIn });
 });
 // Product.sequelize.sync({ force: true, logging: console.log })
 // setup relationship add one to many relationship
--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 const isAuth: RequestHandler = (req, res, next) => {
 	if (!req.session.isLoggedIn) {
-		return res.redirect('/login');
+		return res.status(401).redirect('/login');
 	}
 	next();
 };
