e1ffb8279143f05d97cf6d4ae5ac287f68ab6014
vietanhcao <vietanhcao1994@gmail.com>
2019-12-11 16:17:33 +0700

    add filter file

--- src/app.ts
+++ src/app.ts
@@ -33,11 +33,19 @@ const fileStorage = multer.diskStorage({
 		cb(null, `${new Date().toISOString()} - ${file.originalname}`);
 	}
 });
+
+const fileFilter = (req, file, cb) => {
+	if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
 app.set('view engine', 'pug');
 app.set('views', getYourPath + '/views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 // app.disable('etag');
 app.use(express.static(path.join(__dirname, 'public'))); //file css
 app.use(
